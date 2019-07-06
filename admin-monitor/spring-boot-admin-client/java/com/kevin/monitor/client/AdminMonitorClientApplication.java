d9a67076b87a52b4996ff9b31c51e8700da9d279
it_lihongmin@163.com <it_lihongmin@163.com>
2019-07-07 01:12:48 +0800

    初始化监控服务

---
+++ admin-monitor/spring-boot-admin-client/java/com/kevin/monitor/client/AdminMonitorClientApplication.java
@@ -0,0 +1,14 @@
+package com.kevin.monitor.client;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMonitorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminMonitorClientApplication.class, args);
+	}
+
+}
